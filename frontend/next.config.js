83dcc6aa96d7e8803cfc8908df6e4a93869a11dc
max <repetitive_routine@protonmail.com>
2023-05-01 18:38:51 +0800

    add local IP for larger network testing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   publicRuntimeConfig: {
-    websocketUrl: 'ws://localhost:5090/ws',
+    websocketUrl: 'ws://192.168.0.153:5090/ws',
     //redirectUri: "https://discord.com/api/oauth2/authorize?client_id=1077474383779606600&redirect_uri=http%3A%2F%2F149.28.167.4%3A5090%2Fauth%2Fredirect&response_type=code&scope=identify%20guilds"
-    redirectUri: "https://discord.com/api/oauth2/authorize?client_id=1077474383779606600&redirect_uri=http%3A%2F%2Flocalhost%3A5090%2Fauth%2Fredirect&response_type=code&scope=identify%20guilds"
+    redirectUri: "https://discord.com/api/oauth2/authorize?client_id=1077474383779606600&redirect_uri=http%3A%2F%2F192.168.0.153%3A5090%2Fauth%2Fredirect&response_type=code&scope=guilds%20identify"
   },
   images: {
     domains: ['img.youtube.com'],
@@ -13,7 +13,7 @@ module.exports = {
     return Promise.resolve([
       {
         source: '/:path*',
-        destination: `http://localhost:5090/:path*`,
+        destination: `http://192.168.0.153:5090/:path*`,
       },
     ]);
   },
