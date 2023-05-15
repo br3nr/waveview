2c0846c2bb742a158b758a63a920363442ae5b73
max <repetitive_routine@protonmail.com>
2023-05-15 20:55:39 +0800

    Handle case when env vars not set

--- bot/app.py
+++ bot/app.py
@@ -8,31 +8,30 @@ from src.music.music_commands import MusicCommands
 from src.routers.auth_router import AuthRouter
 from src.routers.player_router import PlayerRouter
 from src.routers.player_ws import PlayerWebsocket
-from config import (
-    BOT_TOKEN,
-    BOT_OAUTH_SECRET,
-    REDIRECT_LOC,
-    REDIRECT_URI,
-    SPOTIFY_CLIENT_ID,
-    SPOTIFY_CLIENT_SECRET
-)
 
-try: # Helps w local testing
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
-    BOT_CLIENT_ID = os.environ["BOT_CLIENT_ID"]
-    BOT_OAUTH_SECRET = os.environ["BOT_OAUTH_SECRET"]
-    SPOTIFY_CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
-    SPOTIFY_CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
-    LAVALINK_URI = os.environ["LAVALINK_URI"]
-except KeyError:
-    LAVALINK_URI = "http://0.0.0.0:2333"
-    pass
+# NOTE: Replace the default None in os.environ.get with the 
+# corresponding value, otherwise set as environment variable
+
+# REQUIRED: Env variables for your bot token, client_id, and client_secret
+# These can be found in the developer portal under Bot and OAuth2 -> General 
+BOT_TOKEN = os.environ.get(["BOT_TOKEN"], None)
+BOT_CLIENT_ID = os.environ.get("BOT_CLIENT_ID", None)
+BOT_CLIENT_SECRET = os.environ.get("BOT_CLIENT_SECRET", None)
+
+# OPTIONAL: Leave as blank if you don't want to use Spotify
+SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
+SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", None)
+
+# REQUIRED: Change these if you plan on hosting thisoutside of localhost
+LAVALINK_URI = os.environ.get("LAVALINK_URI", default="http://0.0.0.0:2333")
+REDIRECT_LOC = os.environ.get("REDIRECT_LOC", default="http://localhost:3000/posts/server-select")
+REDIRECT_URI = os.environ.get("REDIRECT_URI", default="http://localhost:5090/auth/redirect")
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 player = Music(bot, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, LAVALINK_URI)
 app = FastAPI(debug=True)
 
-auth_router = AuthRouter(BOT_TOKEN, BOT_OAUTH_SECRET, REDIRECT_URI, REDIRECT_LOC)
+auth_router = AuthRouter(BOT_TOKEN, BOT_CLIENT_SECRET, REDIRECT_URI, REDIRECT_LOC)
 music_router = PlayerRouter(bot, player)
 player_ws = PlayerWebsocket(bot, player)
 
