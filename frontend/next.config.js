f98a99a5f835da52754e0faeef5235de7f914669
max <repetitive_routine@protonmail.com>
2023-05-19 16:43:07 +0800

    Fix docker-compose name resolution bug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   output: 'standalone',
   publicRuntimeConfig: {
-    websocketUrl: 'ws://localhost:5090/ws',
+    websocketUrl: 'ws://botapi:5090:5090/ws',
     //redirectUri: "https://discord.com/api/oauth2/authorize?client_id=1077474383779606600&redirect_uri=http%3A%2F%2F149.28.167.4%3A5090%2Fauth%2Fredirect&response_type=code&scope=identify%20guilds"
-    redirectUri: "https://discord.com/api/oauth2/authorize?client_id=1077474383779606600&redirect_uri=http%3A%2F%2Flocalhost%3A5090%2Fauth%2Fredirect&response_type=code&scope=identify"
+    redirectUri: "https://discord.com/api/oauth2/authorize?client_id=1077474383779606600&redirect_uri=http%3A%2F%2Flocalhost%3A5090%2Fauth%2Fredirect&response_type=code&scope=identify%20guilds"
   },
   images: {
     domains: ['img.youtube.com'],
@@ -14,7 +14,7 @@ module.exports = {
     return Promise.resolve([
       {
         source: '/:path*',
-        destination: `http://localhost:5090/:path*`,
+        destination: `http://botapi:5090/:path*`,
       },
     ]);
   },
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,28 +1,28 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-import { ChakraProvider, Image } from '@chakra-ui/react'
-import { Button, Center, Text, Flex, Box } from '@chakra-ui/react'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { ChakraProvider, Image } from "@chakra-ui/react";
+import { Button, Center, Text, Flex, Box } from "@chakra-ui/react";
 import Cookies from "js-cookie";
-import { useRouter } from 'next/router'
-import Login from '../components/LoginGraphic/loginGraphic';
-import getConfig from 'next/config';
+import { useRouter } from "next/router";
+import Login from "../components/LoginGraphic/loginGraphic";
+import getConfig from "next/config";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   const { publicRuntimeConfig } = getConfig();
 
-  const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI || publicRuntimeConfig.redirectUri;
+  const redirectUri =
+    process.env.NEXT_PUBLIC_REDIRECT_URI || publicRuntimeConfig.redirectUri;
 
-  async function checkIsLoggedIn()
-  {
+  async function checkIsLoggedIn() {
     const sessionId = Cookies.get("session_id");
     if (sessionId) {
       const response = await fetch(`/auth/login/${sessionId}`);
       if (!response.ok) {
         router.push(redirectUri);
       } else {
-        router.push('/posts/server-select')
+        router.push("/posts/server-select");
       }
     } else {
       router.push(redirectUri);
@@ -30,17 +30,25 @@ export default function Home() {
   }
 
   return (
-    <div >
-      <Flex direction={'column'} alignItems={'center'} justifyContent={'center'} height={'100vh'}>
-      <h1 className="title">
-        <Center>
-          <Text marginBottom={3} fontFamily="mono">wavebot music dashboard</Text>
-        </Center>
-        <Box onClick={() => checkIsLoggedIn()}>
-          <Login/>
-        </Box>
-      </h1>
+    <div>
+      <title>wavebot</title>
+      <Flex
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        height={"100vh"}
+      >
+        <h1 className="title">
+          <Center>
+            <Text marginBottom={3} fontFamily="mono">
+              wavebot music dashboard
+            </Text>
+          </Center>
+          <Box onClick={() => checkIsLoggedIn()}>
+            <Login />
+          </Box>
+        </h1>
       </Flex>
     </div>
-  )
+  );
 }
