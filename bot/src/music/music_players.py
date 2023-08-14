7c6b53f10165b595c8ba76c15580a292dda1da7e
max <repetitive_routine@protonmail.com>
2023-08-14 20:42:27 +0800

    implement youtube playlist

--- bot/src/music/music_players.py
+++ bot/src/music/music_players.py
@@ -44,10 +44,10 @@ class YoutubePlayer(MusicPlayer):
     async def play_track(self, query: str, vc: CustomPlayer, middlequeues):
         # remove time from youtube link
         query = re.sub(r"&t=\d+", "", query)
-        track = await wavelink.NodePool.get_node().get_tracks(
+        tracks = await wavelink.NodePool.get_node().get_tracks(
             wavelink.YouTubeTrack, query
         )
-        track = track[0]
+        track = tracks[0]
         guild_id = str(vc.guild.id)
         cur_queue = middlequeues.get(guild_id, [])
         if vc.is_playing() or not vc.queue.is_empty:
@@ -57,9 +57,23 @@ class YoutubePlayer(MusicPlayer):
             await vc.play(track)
         middlequeues[guild_id] = cur_queue
 
-    async def play_playlist(ctx: discord.ext.commands.Context, playlist: str):
-        # play youtube playlist
-        pass
+    async def play_playlist(self, query: str, vc: CustomPlayer, middlequeues):
+        # Plays a yt playlist
+        # TODO: Make this fast
+        playlist = await wavelink.NodePool.get_node().get_playlist(
+            wavelink.YouTubePlaylist, query
+        )
+        tracks = playlist.tracks
+        guild_id = str(vc.guild.id)
+        cur_queue = middlequeues.get(guild_id, [])
+        for track in tracks:
+            print(track)
+            if vc.is_playing() or not vc.queue.is_empty:
+                vc.queue.put(item=track)
+                cur_queue.append(MiddleQueue(track))
+            else:
+                await vc.play(track)
+            middlequeues[guild_id] = cur_queue
 
 
 class SpotifyPlayer(MusicPlayer):
--- bot/todo.md
+++
@@ -1,4 +0,0 @@
-TODO: 
-
-- Send session ID in http cookie, not url param.
-- Youtube playlist feature
