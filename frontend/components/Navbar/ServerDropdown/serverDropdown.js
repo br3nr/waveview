498befd8e00d7ae0f101d4e423ce34f5bbcdb050
max <repetitive_routine@protonmail.com>
2023-08-20 21:03:43 +0800

    implement chakra menu for discord server select

--- frontend/components/Navbar/ServerDropdown/serverDropdown.js
+++ frontend/components/Navbar/ServerDropdown/serverDropdown.js
@@ -1,15 +1,37 @@
-import { Center, Image, Box, useColorMode, Button } from "@chakra-ui/react";
+import { Image, Menu, MenuButton, MenuItem, MenuList, Button } from "@chakra-ui/react";
 import React from "react";
 import { useState, useEffect } from "react";
 
-function ServerDropdown({}) {
+function ServerDropdown({serverList, setSelectedServer}) {
+
   useEffect(() => {}, []);
 
   return (
     <>
-      <Image src="/discord.png" w="40px" h="40px">
-        
-      </Image>
+      <Menu>
+        <MenuButton  w="40px" h="40px" bgImage="/discord.png" bgSize="40px" as={Button}>
+        </MenuButton>
+        <MenuList>
+          <MenuItem minH="48px">
+            <Image
+              boxSize="2rem"
+              borderRadius="full"
+              src=""
+              mr="12px"
+            />
+            <span>Server Here</span>
+          </MenuItem>
+          <MenuItem minH="40px">
+            <Image
+              boxSize="2rem"
+              borderRadius="full"
+              src=""
+              mr="12px"
+            />
+            <span>Server Here</span>
+          </MenuItem>
+        </MenuList>
+      </Menu>
     </>
   );
 }
